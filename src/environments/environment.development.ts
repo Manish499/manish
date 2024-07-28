7d7928da08bebab2899e1b82187d862d029433b9
Manish499 <manishmishra499@gmail.com>
2024-07-28 16:59:52 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,23 @@
 export const environment = {
-    API_KEY: ''
+    PROJECT_ID: 'vertex-ai-with-angular-sample',
+    GCLOUD_AUTH_PRINT_ACCESS_TOKEN: 'ya29.a0AXooCgs1dsrKzNjaCpyU99WEOj5zkcjlQsOCJFst8NBozvlc2qtIz04wCjS8ixSmty-D4ALcwkJyFkw0La5_8vZ_Sd43AuYVmWkbjem9XVIMcXko2sLof2RdxBBp_8VanNVxjS7GTO7fjhTTX2mMN-HJapNgDUClU6le0QaCgYKASgSARASFQHGX2Mi3pwUCPh1wz8Lhg8clDNpuw0173', // Add gClound print access token
+    // BASE_URL: 'https://asia-south1-aiplatform.googleapis.com/',
+    BASE_URL: 'https://us-central1-prediction-aiplatform.googleapis.com/',
+    // MODEL: 'codestral'
+    MODEL: 'code-bison@002'
+    // MODEL: 'gemini-1.0-pro-vision-001'
+
+
+//     "type": "service_account",
+//   "project_id": "vertex-ai-with-angular-sample",
+//   "private_key_id": "c222b76b896047af44613fd74b93db264316758e",
+//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCULPa4ANBKexet\nvDwGX5mJE+df9J9HLrDGXjvxPOPhBlSJ2kZDhsfLt6LTl0JC0HvfrSn4JjLvEkmP\nsHVHfcgd2iz9MvKC64LbYeqTzxbPUdyRa0alyjKnt2EQeX3kSzz2u94ZLyKah3lH\ndVARzJ5o3qpia/Zw5WUv2uYr13xlePGUUc7RmwsGwStiuIb5lB7sCJL5uH64s+T/\n85mz6CpHkznVJVveOKNys/sZ8xw6F2L0Ahpp4qhSaBXxgt3UWuwgvGK1T4A/Lup7\ncgi5g/Sl7VrsTaBWpmV5yOCKXdS0ch0xpicnPuFGz2GKmLe55dn8zDWlMOABkIb6\n4seSxNcTAgMBAAECggEABh8CuiszxOqSLy0oKviRYSZKAp7FDa28w7+/fixjxYym\nEA53pJvRbnWm66gYpqXyLWqmjIfCHDox1mvAYd8CVf6zU7+3tkzVr7Zlzbavsd98\nhc1cB8ad58Edk2P8HWLg9oMK88NiDJGCzb7nkIoBaUJO4O37f5fFDFW1+HhYZ+Cn\nvURm9ZEDQHnZ0gXXTBbpCSaVm/y32YFYujz7PdtBqlFObTc3fjhNJ04ggryG3Aj6\n781NgMUgv2WgI9eyxyyOGOyNGDi/W30x0ImFN5ZQ30+tj8tP2t6olK/SMcDio21h\nYyWN2f80rHjYpO3R7HnOnieaJjG4Mg/nGdNr0NKDUQKBgQDENq0reWISfnwbWrJQ\nLZ3anHvfCAiDnezqK+MBvd+J/0H02BaVLWpVzldnfdADW5xctaQHbuRB3ufJsbzn\nGLCdbfAYMbOvd581/sLAbYK3SlToj/qKoQ1/HKkW7Hz4PfNPzsjMzL92foNeiEvJ\nkPuLwL6GMHGgs/HCrPMPMfAZRQKBgQDBUypSV8ivREXapkiIL6tlhr6OwqEupisw\nc1P306Fp+8J6xi9M07+srbJDeBthsEnTTCZ6CrGaB7uZFJtqxI3Buu6E8N6dAJ9H\nRDOv0+W2HLGkhrrhLlSuUj5XTghJTRO/qC5jqaMo39sRJqFCKZIzQfVS3xsOyLmz\nT0Yd+To4dwKBgCnjXk7EqBo4pHgWgbC6PFPaSluP1Hl8soI/gkPx7BICy2rg4tlQ\nC75rwZ8Xbdv5p8TnBjCMDPC/DI6zghd33uSM/s+7dJfGGtglHiCEgH5uwPexHRSR\nU1dKoBKDxB4e452rEJUgNNOwKI163WsXp4vrRwFQwEFFiRw9Xdpll9NdAoGAEJcK\ncraFykSZmgHbB5TPqI6/m5CoNFCeNDIxansGxwmLzu1i8bzaHf9p5NBRyThPnYpT\nYvW39/fh6WkTTT5Ay/c2sMHa2K5M/NildtlARTiBJhVJxDktmygDoYpYu7GZmEDe\nej78eW18ZyRhgkP+/9cRLmNm9wXSqciLw9a3YFECgYBUEu6Bp7y+VBwNHFRFyghT\nKwfhEM1KksMlJC3oL7HS/8oI+O2gevs+5oTPgcm/tEcnBHZMV+jaZKUlJz8Zdgcp\nmdRJhLU0L0+Na44kvZqNM43MqlkxefKxPYVgTCRmElwozN87rcNOejDFBIPA5jHe\n1+0+dpKPvH2wrCRfbtevPQ==\n-----END PRIVATE KEY-----\n",
+//   "client_email": "vertexaiapiwithangular@vertex-ai-with-angular-sample.iam.gserviceaccount.com",
+//   "client_id": "106896497539998779463",
+//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//   "token_uri": "https://oauth2.googleapis.com/token",
+//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/vertexaiapiwithangular%40vertex-ai-with-angular-sample.iam.gserviceaccount.com",
+//   "universe_domain": "googleapis.com"
+
 };
